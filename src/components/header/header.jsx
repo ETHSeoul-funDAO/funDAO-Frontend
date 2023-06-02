f1e7cd22a2ebbbe875b7e770d89d2379d0f193b9
한성원 <holyhansss@gmail.com>
2023-06-03 02:08:09 +0900

    Main Page

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,8 +9,22 @@ import Navbar from 'react-bootstrap/Navbar';
 const Header = () => {
     const [account, setAccount] = useState("");
     const [displayAccount, setDisplayAccount] = useState("");
-    
+    const circleStyle = "d-inline-flex bg-dark border border-white-50 text-white-50 rounded-2 mx-2 py-1";
 
+    const changeNetwork = async () => {
+        if (window.ethereum) {
+          try {
+            await window.ethereum.enable();
+            window.ethereum._handleChainChanged({
+              chainId: 0x1,
+              networkVersion: 1,
+            });
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      };
+    
     const connectToMetamask = async() =>  {
         if(window.ethereum){
             try {
@@ -29,7 +43,7 @@ const Header = () => {
   
     return (
         <>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark pt-3" variant="dark">
                 <Container >
                     <Navbar.Brand href="/">FunDAO</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
@@ -41,9 +55,22 @@ const Header = () => {
                         </Nav>
                         {
                             account === "" 
-                            ? <button onClick={connectToMetamask}>MetaMask</button>
-                            : <button>{displayAccount}</button>
+                            ? <button class={circleStyle} onClick={connectToMetamask}>Connect MetaMask</button>
+                            : <div>
+                                <div class="dropdown">
+                                    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Change Network
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Aurora</a></li>
+                                        <li><a class="dropdown-item" href="#">ETH</a></li>
+                                        <li><a class="dropdown-item" href="#">what?</a></li>
+                                    </ul>
+                                    <button class={circleStyle}>{displayAccount}</button>
+                                </div>
+                            </div>
                         }
+                        
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import Header from "../components/header/header";
+import { Container } from "react-bootstrap";
 
-export default function MainPage() {
-  return (
-      <Header></Header>
-  );
+const MainPage = () =>{
+
+    const circleStyle = "d-inline-flex border border-white-50 text-white-50 rounded-pill ps-2 pe-2 ms-1 me-2";
+    const Theme = "Fund your thing";
+    const desc = "Become part of a vast community of projects leveraging fundDao to transparently fund, operate, and expand their ideas and communities on the various network.";
+
+
+    const renderProjectCards = () => {
+        const result = [];
+        for(let i=0; i<4; i++){
+            result.push(<div class="card mx-3" style={{width: '18rem'}} key={i}>
+                <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>);
+        }
+        return result;
+    }
+
+    return (
+        <div class="bg-dark">
+            <Header></Header>
+            <div>
+                <div class="row mt-5 pt-3">
+                    <div>
+                        <div class={circleStyle}>Concert</div>
+                        <div class={circleStyle}>Movie</div>
+                        <div class={circleStyle}>Fundraising</div>
+                        <div class={circleStyle}>Art</div>
+                        <div class={circleStyle}>Business</div>
+                        <div class={circleStyle}>All →</div>
+                    </div>
+                </div>
+            <div class="text-white fw-bolder fst-italic display-1 mt-5">
+            {Theme}
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="text-white-50 ps-5 pe-5 mt-5" style={{width:'40%'}}>
+                    {desc}
+                </div>
+            </div>
+                <button type="button" class="btn btn-primary mt-4 px-4 py-3" style={{'background-color': '#3771e6'}}>Create a project</button>
+            </div>
+            <div class="row justify-content-md-center mt-5">
+                {renderProjectCards()}
+            </div>
+            <div class="row justify-content-md-center mt-3">
+                {renderProjectCards()}
+            </div>
+        </div>
+    );
 }
+
+export default MainPage;
