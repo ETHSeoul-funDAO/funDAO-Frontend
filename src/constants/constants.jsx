1af0172e5c7a293201a65c0ff49416464a150ec4
한성원 <holyhansss@gmail.com>
2023-06-03 17:19:45 +0900

    adding networks and create page

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,7 +1,5 @@
-
-
 // Aurora
-export const AURORAPARAM = [{
+export const AURORA_PARAM = [{
     "chainId": "0x4e454152", // 1313161554 in decimal
      // testnet alternative: "chainId": "0x4e454153", // 1313161555 in decimal	
     "chainName": "Aurora",
@@ -16,4 +14,31 @@ export const AURORAPARAM = [{
     "blockExplorerUrls": [
         // not yet available
     ]
-}] 
+}];
+
+export const CHAINIDS_DEC = {
+    polygon: 137,
+    gnosisTestnet_Chiado: 10200,
+    auroraTestent: 1313161555,
+
+}
+export const PARAMS = {
+    polygon: [{
+        chainName: 'Polygon Mainnet',
+        chainId: "0x" + CHAINIDS_DEC.polygon.toString(16),
+        nativeCurrency: { name: 'MATIC', decimals: 18, symbol: 'MATIC' },
+        rpcUrls: ['https://polygon-rpc.com/']
+    }],
+    gnosisTestnet_Chiado: [{
+        chainName: 'gnosis Chiado testnet',
+        chainId: "0x" + CHAINIDS_DEC.gnosisTestnet_Chiado.toString(16),
+        nativeCurrency: { name: 'XDAI', decimals: 18, symbol: 'XDAI' },
+        rpcUrls: ['https://rpc.chiadochain.net']
+    }],
+    auroraTestent: [{
+        chainName: 'Aurora Testent',
+        chainId: "0x" + CHAINIDS_DEC.auroraTestent.toString(16),
+        nativeCurrency: { name: 'Ether', decimals: 18, symbol: 'ETH' },
+        rpcUrls: ['https://aurora-testnet.rpc.thirdweb.com']
+    }]
+}
